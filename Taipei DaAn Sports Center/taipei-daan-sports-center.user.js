12d26dbb0291c321ab986df03ecd78628eebaaa2
Brian Hu <b159951519@gmail.com>
2023-11-28 17:06:31 +0800

    Update taipei-daan-sports-center.user.js
    1. Update the matching URL
    2. fix the layout

--- Taipei DaAn Sports Center/taipei-daan-sports-center.user.js
+++ Taipei DaAn Sports Center/taipei-daan-sports-center.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         大安運動中心排版修改
 // @namespace    https://github.com/BrianHuGit/Tampermonkey-scripts
-// @version      1.0
+// @version      1.1
 // @description  改善預約場地的排版畫面，支援羽球、桌球、撞球、壁球
 // @author       Brian Hu
-// @match        https://scr.cyc.org.tw/tp03.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1*
-// @match        https://scr.cyc.org.tw/tp03.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=3*
-// @match        https://scr.cyc.org.tw/tp03.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=4*
-// @match        https://scr.cyc.org.tw/tp03.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=8*
+// @match        https://www.cjcf.com.tw/CG02.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1*
+// @match        https://www.cjcf.com.tw/CG02.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=3*
+// @match        https://www.cjcf.com.tw/CG02.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=4*
+// @match        https://www.cjcf.com.tw/CG02.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=8*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @license      WTFPL License
@@ -17,18 +17,24 @@
     'use strict';
     $(document).ready(function () {
 
-        let style = '<style>table {border-style: solid; border-color: rgb(200, 200, 200);}\ndiv.preserve_btn {height: 36px;display: table-cell;vertical-align: middle;font-size: 14px;color: #1E5EDE;cursor: pointer;}\ndiv.preserve_btn:hover {color: #578AEF;text-decoration: underline;}</style>'
+        let style = '<style>table {border-style:solid; border-color:rgb(200, 200, 200);}\ndiv.preserve_btn {display:flex; justify-content:center; align-items:center;height:36px;font-size:14px;color:#1E5EDE;cursor:pointer;}\ndiv.preserve_btn:hover {color:#578AEF;text-decoration:underline;}</style>'
         $('head').append(style);
 
         var calendar = {};
-        var place_set = new Set();
+        var place_name_set = new Set();
+        let time;
         $('div#ContentPlaceHolder1_Panel_Step2 td.tWord tr').not(':first-child').each(function () {
             $(this).children().removeAttr('valign');
-            let time = $(this).children('td').eq(0).text().replace(/\s/g, '');
-            let place = $(this).children('td').eq(1).text().replace(/\s/g, '');
-            let price = $(this).children('td').eq(2).text().replace(/\s/g, '');
-            let button = $(this).children('td').eq(3);
-            if (button.children('img').attr('src') == 'img/sche02.jpg') {
+            // The first row will have 4 columns, the first column is time
+            if ($(this).children().length == 4) {
+                time = $(this).children('td').eq(0).text().replace(/\s/g, '');
+                $(this).children('td').eq(0).remove(); // remove the time column then the next column index will be 0
+            }
+            let place = $(this).children('td').eq(0).text().replace(/\s/g, '');
+            let price = $(this).children('td').eq(1).text().replace(/\s/g, '');
+            let button = $(this).children('td').eq(2);
+
+            if (button.children('img').attr('src') == 'img/place02.png') {
                 button.empty().append('-');
             }
             else {
@@ -36,55 +42,51 @@
                 let div = '<div class="preserve_btn" onclick="' + onclick + '">預約</div>';
                 button.empty().append(div);
             }
-
-            place_set.add(place);
+            place_name_set.add(place);
             if (!(time in calendar)) calendar[time] = {};
             calendar[time][place] = { 'price': price, 'button': button };
         });
 
-        var place_list = Array.from(place_set);
-        $('div#ContentPlaceHolder1_Panel_Step2 td.tWord tr').each(function (index) {
-            // handle the case if table column count is less than place count
-            let column_count = $(this).children('td').not(':first-child').length;
-            while (place_list.length != column_count) {
-                if (place_list.length > column_count) {
-                    $(this).append($(this).children('td:last-child').prop('outerHTML'));
-                }
-                if (place_list.length < column_count) {
-                    $(this).children('td:last-child').remove();
-                }
-                column_count = $(this).children('td').not(':first-child').length;
-            }
-            $(this).css('height', '36px');
+        var place_name_list = Array.from(place_name_set);
+        // Remove the content of the table
+        $('div#ContentPlaceHolder1_Panel_Step2 table table:last-child tbody')[1].remove()
 
-            // first row
-            if ($(this).is(':first-child')) {
-                $(this).children('td').each(function (index) {
-                    $(this).text(place_list[index - 1]);
-                    $(this).css('border', 'none');
-                });
-            }
-            // the others row
-            else {
-                // remove duplicate row
-                if (index % place_list.length != 1) {
-                    $(this).remove();
-                }
-                else {
-                    let time = $(this).children('td').eq(0).text();
-                    $(this).children('td').each(function (index) {
-                        // except the first child
-                        if ($(this).is(':not(:first-child)')) {
-                            let place = place_list[index - 1];
-                            $(this).empty().append(calendar[time][place]['button'].html());
-                        }
-                        $(this).css({ 'border-style': 'solid', 'border-width': '1px 0px 0px 0px', 'border-color': 'rgb(200, 200, 200)' });
-                    });
-                }
-            }
-        });
+        const table = $('div#ContentPlaceHolder1_Panel_Step2 table table:last-child')[1];
+        const thead = document.createElement('thead');
+        const tr = document.createElement('tr');
+        const th = document.createElement('th');
+        th.style.textAlign = 'center';
+        th.style.verticalAlign = 'middle';
+        th.innerText = '時間';
+        tr.append(th);
+        place_name_list.forEach(function (place_name) {
+            const th = document.createElement('th');
+            th.style.textAlign = 'center';
+            th.style.verticalAlign = 'middle';
+            th.innerText = place_name;
+            tr.append(th);
+        })
+        thead.append(tr);
+        table.append(thead);
 
-        // remove ugly footer
-        $('table').has('div#footer').remove()
+        const tbody = document.createElement('tbody')
+        for (const [time, prices] of Object.entries(calendar)) {
+            const tr = document.createElement('tr');
+            const td = document.createElement('td');
+            td.style.textAlign = 'center';
+            td.style.verticalAlign = 'middle';
+            td.innerText = time;
+            tr.append(td);
+            place_name_list.forEach(function (place_name) {
+                const td = document.createElement('td');
+                td.style.textAlign = 'center';
+                td.style.verticalAlign = 'middle';
+                td.innerHTML = prices[place_name]["button"].html();
+                tr.append(td);
+            })
+            tbody.append(tr)
+        }
+        table.append(tbody);
+
     });
 })();
